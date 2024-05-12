a87afc9e8c009c3c3da80a9f4f08552a31ff638b
mahdi niknejad <m.niknejad@aut.ac.ir>
2024-05-12 07:48:23 -0700

    Add pyotp models [WIP]

--- monitoring/account/models.py
+++ monitoring/account/models.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-
+import pyotp
 
 # Create your models here.
 
 class User(AbstractUser):
     avatar = models.CharField(max_length=20, verbose_name='تصویر آواتار', null=True, blank=True)
     email_active_code = models.CharField(max_length=100, verbose_name='کد فعالسازی ایمیل')
+    otp = models.CharField(max_length=6)
+    is_two_factor_authentication_enabled = False
 
     class Meta:
         verbose_name = 'کاربر'
